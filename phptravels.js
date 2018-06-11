d2f31e809bbe39bb13d6848c3346befb65a137c4
Clement Cherlin <clement@case>
2018-06-11 18:05:00 -0400

    Update and save profile information

--- phptravels.js
+++ phptravels.js
@@ -14,9 +14,29 @@ function waitUntilStale(element) {
 			//console.log(err.message);
 			return err.name === 'StaleElementReferenceError'
 		});
-	});
+	}, 20000);
 }
-
+function waitUntilPresent(selector) {
+    return driver.wait(function() {
+        return driver.findElement(selector).then(function(element) {
+            return element.isDisplayed().then(function(displayed) {
+                displayed ? element : null
+            });
+        },
+        function(err) {
+            console.log(err.name);
+			//console.log(err.message);
+            return null;
+        });
+    }, 15000);
+}
+function sendKeysToName(name, text) {
+    return driver.findElement(By.name(name)).then(function(namedElement) {
+        namedElement.clear().then(function() {
+            namedElement.sendKeys(text);
+        });
+    });
+}
 driver.get('https://www.phptravels.net/').then(function() {
     return driver.findElement(By.xpath("(//a[contains(text(),'My Account')])[2]")).then(function(myAccount) {
         myAccount.click();
@@ -33,6 +53,31 @@ driver.get('https://www.phptravels.net/').then(function() {
 }).then(function() {
     driver.findElement(By.name("password")).then(function(password) {
         password.sendKeys("demouser", webdriver.Key.ENTER);
-        waitUntilStale(password);
+        waitUntilStale(password)
+    })
+}).then(function() {
+    driver.sleep(5000).then(function() {
+        driver.findElement(By.css("span.profile-icon")).then(function(profile) {
+            driver.sleep(5000).then(function() {
+                profile.click();
+            });
+            console.log("Clicked on profile!");
+        });
+    });
+}).then(function() {
+    sendKeysToName("address1", "469 AZ-64");
+//    driver.findElement(By.name("address1")).then(function(address1) {
+//        address1.sendKeys("469 AZ-64");
+//    })
+}).then(function() {
+    //driver.findElement(By.name("city")).then(function())
+    sendKeysToName("city", "Grand Canyon Village");
+}).then(function() {
+    sendKeysToName("state", "AZ");
+}).then(function() {
+    sendKeysToName("zip", "86023");
+}).then(function() {
+    driver.findElement(By.css("button.updateprofile")).then(function(updateButton) {
+        updateButton.click();
     });
 });
